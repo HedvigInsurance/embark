b79a24246829edc8c9c21627e796b8959ede8d60
Oscar Nylander <oscar@hedvig.com>
2019-10-29 15:03:32 +0100

    PR changes

--- src/Components/Response/GroupedResponse.tsx
+++ src/Components/Response/GroupedResponse.tsx
@@ -1,42 +1,52 @@
-import * as React from 'react'
-import { ExpressionTextNode, getTextContent, replacePlaceholders, MessageBody } from '../Common'
-import { StoreContext } from '../KeyValueStore'
-import styled from '@emotion/styled'
-import { colors, fonts } from '@hedviginsurance/brand'
-import { Colors } from '../../colors'
+import * as React from "react";
+import {
+  ExpressionTextNode,
+  getTextContent,
+  replacePlaceholders,
+  MessageBody
+} from "../Common";
+import { StoreContext } from "../KeyValueStore";
+import styled from "@emotion/styled";
+import { colorsV2, fonts } from "@hedviginsurance/brand";
 
 const Title = styled.p`
-    color: ${colors.WHITE};
-    font-family: ${fonts.CIRCULAR};
-    font-weight: bold;
-`
+  color: ${colorsV2.white};
+  font-family: ${fonts.CIRCULAR};
+  font-weight: bold;
+`;
 
 const ItemContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 const Item = styled.span`
-    background-color: ${Colors.violet700};
-    border-radius: 18px;
-    padding: 8px;
-    margin: 4px;
-`
+  background-color: ${colorsV2.violet700};
+  border-radius: 18px;
+  padding: 8px;
+  margin: 4px;
+`;
 
 interface Props {
-    title: ExpressionTextNode,
-    items: ExpressionTextNode[]
+  title: ExpressionTextNode;
+  items: ExpressionTextNode[];
 }
 
-export const GroupedResponse: React.FunctionComponent<Props> = (props) => (
-    <StoreContext>
-        {({store}) => (
-            <MessageBody isResponse={true}>
-                <Title>{replacePlaceholders(store, getTextContent(store, props.title))}</Title>
-                <ItemContainer>{props.items.map(item => (
-                    <Item>{replacePlaceholders(store, getTextContent(store, item))}</Item>
-                ))}</ItemContainer>
-            </MessageBody>
-        )}
-    </StoreContext>
-)
+export const GroupedResponse: React.FunctionComponent<Props> = props => (
+  <StoreContext>
+    {({ store }) => (
+      <MessageBody isResponse={true}>
+        <Title>
+          {replacePlaceholders(store, getTextContent(store, props.title))}
+        </Title>
+        <ItemContainer>
+          {props.items.map(item => (
+            <Item>
+              {replacePlaceholders(store, getTextContent(store, item))}
+            </Item>
+          ))}
+        </ItemContainer>
+      </MessageBody>
+    )}
+  </StoreContext>
+);
--- src/colors.ts
+++
@@ -1,4 +0,0 @@
-export const Colors = {
-  pink: "#F88C76",
-  violet700: "#5018cc"
-};
