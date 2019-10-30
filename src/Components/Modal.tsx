66a75f199e71cc283cbc00e4e754a58ab34df5ad
Gustaf Gunér <gustaf@guner.se>
2019-10-30 16:03:19 +0100

    Animations and close button

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -2,13 +2,15 @@ import * as React from "react";
 import styled from "@emotion/styled";
 import { colorsV2 } from "@hedviginsurance/brand";
 import hexToRgba = require("hex-to-rgba");
+import { motion } from "framer-motion";
+import { Cross } from "../Components/Icons/Cross";
 
 interface ModalProps {
   isVisible: boolean;
   onClose: () => void;
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   position: fixed;
   width: 100%;
   height: 100%;
@@ -19,7 +21,9 @@ const Wrapper = styled.div`
   background-color: ${hexToRgba(colorsV2.white, 0.75)};
 `;
 
-const Container = styled.div`
+const Container = styled(motion.div)`
+  position: relative;
+
   width: 500px;
   padding: 24px;
   max-width: calc(100% - 32px);
@@ -36,6 +40,35 @@ const Container = styled.div`
   overflow-x: scroll;
 `;
 
+const CloseButton = styled.button`
+  width: 26px;
+  height: 26px;
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  background-color: ${colorsV2.gray};
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+
+  :focus {
+    outline: none;
+  }
+
+  :hover {
+    background-color: ${colorsV2.darkgray};
+  }
+
+  svg {
+    width: 100%;
+    height: 100%;
+    fill: ${colorsV2.white};
+  }
+`;
+
 export const Modal = (props: React.PropsWithChildren<ModalProps>) => {
   const containerRef = React.useRef(null);
 
@@ -47,11 +80,50 @@ export const Modal = (props: React.PropsWithChildren<ModalProps>) => {
     return () => document.removeEventListener("mousedown", handleClick);
   }, []);
 
-  return props.isVisible ? (
-    <Wrapper>
-      <Container ref={containerRef}>{props.children}</Container>
+  return (
+    <Wrapper
+      initial={"hidden"}
+      animate={props.isVisible ? "visible" : "hidden"}
+      transition={{
+        type: "spring"
+      }}
+      variants={{
+        visible: {
+          opacity: 1,
+          visibility: "visible"
+        },
+        hidden: {
+          opacity: 0,
+          visibility: "hidden"
+        }
+      }}
+    >
+      <Container
+        ref={containerRef}
+        initial={"hidden"}
+        animate={props.isVisible ? "visible" : "hidden"}
+        transition={{
+          type: "spring",
+          stiffness: 400,
+          damping: 25,
+          delay: 0.05
+        }}
+        variants={{
+          visible: {
+            opacity: 1,
+            transform: "translateX(-50%) translateY(-50%) scale(1)"
+          },
+          hidden: {
+            opacity: 0,
+            transform: "translateX(-50%) translateY(-50%) scale(0.9)"
+          }
+        }}
+      >
+        {props.children}
+        <CloseButton onClick={() => props.onClose()}>
+          <Cross />
+        </CloseButton>
+      </Container>
     </Wrapper>
-  ) : (
-    <></>
   );
 };
