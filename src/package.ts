8569f493d1d8f771a9d5d8e92bb823236f618019
Oscar Nylander <oscar@hedvig.com>
2019-11-25 11:13:58 +0100

    Export API types
    Bonus: Clean up GraphQL cruft

--- src/package.ts
+++ src/package.ts
@@ -9,3 +9,15 @@ export { Passage } from "./Components/Passage";
 export { Header } from "./Components/Header";
 export { useGoTo } from "./Utils/ExpressionsUtil";
 export { EmbarkProvider } from "./Components/EmbarkProvider";
+
+export {
+  Data as PersonalInformationData
+} from "./Components/API/personalInformation";
+export {
+  Data as HouseInformationData,
+  Variables as HouseInformationVariables
+} from "./Components/API/houseInformation";
+export {
+  Data as CreateQuoteData,
+  Variables as CreateQuoteVariables
+} from "./Components/API/createQuote";
