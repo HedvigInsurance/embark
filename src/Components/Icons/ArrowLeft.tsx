ef4dad8f3da5bdfd5d2f4680d25696482d543998
Sam Pettersson <sam@sampettersson.com>
2019-12-11 13:17:11 +0100

    Update src/Components/Icons/ArrowLeft.tsx
    Co-Authored-By: hedvigoscar <39644124+hedvigoscar@users.noreply.github.com>

--- src/Components/Icons/ArrowLeft.tsx
+++ src/Components/Icons/ArrowLeft.tsx
@@ -4,7 +4,7 @@ export const ArrowLeft = () => (
   <svg width="12" height="19" xmlns="http://www.w3.org/2000/svg">
     <path
       stroke="#9B9BAA"
-      stroke-width="1.2"
+      strokeWidth="1.2"
       d="M10 1.624L9.367 1 1 9.25l8.367 8.25.633-.624L2.265 9.25z"
       fill="none"
       fillRule="evenodd"
