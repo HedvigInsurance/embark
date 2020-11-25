cb8601668a66cffaee08bec60c767117420744c1
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-11-25 16:21:58 +0100

    one file to rule them all

--- src/Resolvers/resolveStoriesMetadata.ts
+++ src/Resolvers/resolveStoriesMetadata.ts
@@ -1,17 +1,9 @@
 import { promises } from 'fs'
 
 export const resolveMetadataOnLocale = async (locale: string) => {
-  switch (locale) {
-    case 'sv_SE': {
-      const file = await promises.readFile(
-        'stories-metadata/Swedish - Sweden - Stories.json',
-        {
-          encoding: 'utf-8',
-        },
-      )
-
-      const json = JSON.parse(file)
-      return json
-    }
-  }
+  const file = await promises.readFile('metadata/stories.json', {
+    encoding: 'utf-8',
+  })
+  const json = JSON.parse(file)
+  return json[locale] || []
 }
