321a5d04ea7dea196d61021c4659c38d3004ab74
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-11-25 16:30:32 +0100

    use path to resolve path

--- src/Resolvers/resolveStoriesMetadata.ts
+++ src/Resolvers/resolveStoriesMetadata.ts
@@ -1,9 +1,14 @@
 import { promises } from 'fs'
 
+const path = require('path')
+
 export const resolveMetadataOnLocale = async (locale: string) => {
-  const file = await promises.readFile('metadata/stories.json', {
-    encoding: 'utf-8',
-  })
+  const file = await promises.readFile(
+    path.resolve(__dirname, '../../metadata/stories.json'),
+    {
+      encoding: 'utf-8',
+    },
+  )
   const json = JSON.parse(file)
   return json[locale] || []
 }
