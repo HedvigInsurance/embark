aa767ce338f91ce1bb2b6d7350aaa611debdf1b6
Robin Andeer <robin@hedvig.com>
2021-07-06 13:49:29 +0200

    Break out Embark store logic from action
    Add new hook to interact with embark store.

--- src/Components/Actions/AddressAutocompleteAction/AddressAutocompleteAction.tsx
+++ src/Components/Actions/AddressAutocompleteAction/AddressAutocompleteAction.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react'
 import { motion } from 'framer-motion'
-import { StoreContext, Store } from '../../KeyValueStore'
 import { Tooltip } from '../../Tooltip'
-import { Card as BaseCard, Input, Spacer } from '../Common'
+import { Card as BaseCard, Spacer } from '../Common'
 import styled from '@emotion/styled'
 import { ApiContext } from '../../API/ApiContext'
 import { ApiComponent } from '../../API/apiComponent'
@@ -21,21 +20,7 @@ import {
 import AddressAutocomplete from './AddressAutocomplete'
 import useAddressSearch from './useAddressSearch'
 import Modal from './Modal'
-
-const ADDRESS_NOT_FOUND = 'ADDRESS_NOT_FOUND'
-
-const STORE_KEY = {
-  ID: 'bbrId',
-  ADDRESS: 'fullAddress',
-  STREET: 'street',
-  STREET_NAME: 'streetName',
-  STREET_NUMBER: 'streetNumber',
-  ZIP_CODE: 'zipCode',
-  CITY: 'city',
-  FLOOR: 'floor',
-  APARTMENT: 'apartment',
-  ADDRESS_SEARCH_TERM: 'addressSearchTerm',
-}
+import useStoreAddress from './useStoreAddress'
 
 const Container = styled.div`
   max-width: 100%;
@@ -109,58 +94,24 @@ export interface AddressAutocompleteActionProps {
   onContinue: () => void
 }
 
-const getAddressFromStore = (store: Store): CompleteAddress | null => {
-  const data: AddressSuggestion = {
-    id: store[STORE_KEY.ID],
-    address: store[STORE_KEY.ADDRESS],
-    streetName: store[STORE_KEY.STREET_NAME],
-    streetNumber: store[STORE_KEY.STREET_NUMBER],
-    floor: store[STORE_KEY.FLOOR],
-    apartment: store[STORE_KEY.APARTMENT],
-    postalCode: store[STORE_KEY.ZIP_CODE],
-    city: store[STORE_KEY.CITY],
-  }
-
-  return isCompleteAddress(data) ? data : null
-}
-
-const collectAddressSubmission = (address: CompleteAddress) => {
-  const data = {
-    [STORE_KEY.ID]: address.id,
-    [STORE_KEY.STREET]: `${address.streetName} ${address.streetNumber}`,
-    [STORE_KEY.ZIP_CODE]: address.postalCode,
-    [STORE_KEY.CITY]: address.city,
-    [STORE_KEY.STREET_NAME]: address.streetName,
-    [STORE_KEY.STREET_NUMBER]: address.streetNumber,
-
-    [STORE_KEY.ADDRESS]: address.address,
-  }
-
-  if (address.floor) {
-    data[STORE_KEY.FLOOR] = address.floor
-  }
-
-  if (address.apartment) {
-    data[STORE_KEY.APARTMENT] = address.apartment
-  }
-
-  return data
-}
-
 export const AddressAutocompleteAction: React.FC<AddressAutocompleteActionProps> = (
   props,
 ) => {
   const api = React.useContext(ApiContext)
 
+  const { saveAddress, saveNotFound, initialAddress } = useStoreAddress({
+    storeKey: props.storeKey,
+    passageName: props.passageName,
+  })
+
   const [isHovered, setIsHovered] = React.useState(false)
-  const { store, setValue, removeValues } = React.useContext(StoreContext)
 
   const [isAutocompleteActive, setIsAutocompleteActive] = React.useState(false)
 
   const [
     confirmedAddress,
     setConfirmedAddress,
-  ] = React.useState<CompleteAddress | null>(() => getAddressFromStore(store))
+  ] = React.useState<CompleteAddress | null>(initialAddress)
 
   const [
     pickedSuggestion,
@@ -214,26 +165,23 @@ export const AddressAutocompleteAction: React.FC<AddressAutocompleteActionProps>
     [confirmSuggestion, pickedSuggestion, setSuggestions],
   )
 
-  const handleChangeInput = React.useCallback(
-    (newValue: string) => {
-      setSearchTerm(newValue)
+  const handleChangeInput = React.useCallback((newValue: string) => {
+    setSearchTerm(newValue)
 
-      // Reset picked suggestion for empty input field
-      if (!newValue) {
-        setPickedSuggestion(null)
+    // Reset picked suggestion for empty input field
+    if (!newValue) {
+      setPickedSuggestion(null)
+    }
+
+    setConfirmedAddress((prevValue) => {
+      // Reset confirmed address unless it matches the updated search term
+      if (prevValue && newValue !== formatAddressLine(prevValue)) {
+        return null
       }
 
-      setConfirmedAddress((prevValue) => {
-        // Reset confirmed address unless it matches the updated search term
-        if (prevValue && newValue !== formatAddressLine(prevValue)) {
-          return null
-        }
-
-        return prevValue
-      })
-    },
-    [store],
-  )
+      return prevValue
+    })
+  }, [])
 
   const handleButtonClick = () => {
     setIsAutocompleteActive(true)
@@ -245,47 +193,19 @@ export const AddressAutocompleteAction: React.FC<AddressAutocompleteActionProps>
     setConfirmedAddress(null)
   }, [])
 
-  const clearStoreValues = React.useCallback(
-    () =>
-      Object.values(STORE_KEY).forEach((storeKey) => {
-        removeValues(storeKey)
-      }),
-    [removeValues],
-  )
-
   const handleContinue = React.useCallback(
     (address: CompleteAddress) => {
-      clearStoreValues()
-
-      Object.entries(collectAddressSubmission(address)).forEach(
-        ([key, value]) => {
-          setValue(key, value)
-        },
-      )
-
-      const addressLine = formatAddressLine(address)
-      setValue(props.storeKey, addressLine)
-      setValue(`${props.passageName}Result`, addressLine)
-
-      return props.onContinue()
+      saveAddress(address)
+      props.onContinue()
     },
-    [
-      clearStoreValues,
-      setValue,
-      props.storeKey,
-      props.passageName,
-      props.onContinue,
-    ],
+    [saveAddress, props.onContinue],
   )
 
   const handleNoAddressFound = React.useCallback(() => {
     setIsAutocompleteActive(false)
-    clearStoreValues()
-
-    setValue(STORE_KEY.ADDRESS_SEARCH_TERM, searchTerm)
-    setValue(props.storeKey, ADDRESS_NOT_FOUND)
+    saveNotFound(searchTerm)
     props.onContinue()
-  }, [clearStoreValues, setValue, props.storeKey, searchTerm])
+  }, [searchTerm, saveNotFound])
 
   const handleDismissModal = React.useCallback(
     () => setIsAutocompleteActive(false),
